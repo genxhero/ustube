96c151dd84068e3e173ddff1976f3dce4b9ec489
genxhero <genxhero@users.noreply.github.com>
2019-11-24 14:04:50 -0800

    create db migration for videos table

--- db/migrate/20191124214807_createvideostable.rb
+++ db/migrate/20191124214807_createvideostable.rb
@@ -1,4 +1,14 @@
 class Createvideostable < ActiveRecord::Migration[6.0]
   def change
+    create_table :videos do |t|
+      t.string title, null: false
+      t.string image_url, null: false
+      t.string video_url, null: false
+      t.text description, null: false 
+      t.integer user_id, null: false
+    end
+        add_index :videos, :title
+        add_index :videos, :user_id
+        add_index :videos, :video_url
   end
 end
