a5c0859a34d8f7f07e190a474a51b20e247307f7
genxhero <genxhero@users.noreply.github.com>
2019-11-25 17:29:32 -0800

    sessions controller

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,22 @@
+
+class SessionsController < ApplicationController
+
+    def create
+        @user = User.find_by_credentials(
+          params[:user][:email],
+          params[:user][:password]
+        )
+        if @user
+          login(@user)
+          render "/users/show"
+        else
+          render json: ["Invalid Credentials"], status: 422
+        end
+    end
+
+    def destroy
+        logout
+        render json: {}
+      end
+      
+end
