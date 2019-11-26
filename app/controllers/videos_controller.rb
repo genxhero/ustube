c41702001568698e829d6953e4f8a6214020541d
genxhero <genxhero@users.noreply.github.com>
2019-11-25 17:19:53 -0800

    video show component skeleton

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,2 +1,14 @@
 class VideosController < ApplicationController
+    
+    def index
+        @videos = Video.all
+        render :index
+    end
+    
+    def create 
+    end
+
+    def show
+        @video = Video.find(params[:id].to_i)
+    end
 end
---
+++ app/javascript/components/VideoShow.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const VideoShow = props => {
+
+}
+
+export default VideoShow;
