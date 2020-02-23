71b0f23cfcf1f94fa1796a0c2dd6141ea8a1b0cc
genxhero <genxhero@users.noreply.github.com>
2020-02-23 09:05:59 -0800

    create a hide button

--- app/javascript/components/Advertisement.jsx
+++ app/javascript/components/Advertisement.jsx
@@ -1,10 +1,14 @@
 import React, {useState} from 'react';
+import {FaArrowAltCircleLeft} from 'react-icons/fa';
 
 const Advertisement = props => {
      const [visible, toggleVisibility] = useState(true)
     return (
         <div className="advertisement">
             <div>Your Ad Here </div>
+            <div className={"hide-btn-left"} onClick={() => toggleVisibility(false)}>
+                <FaArrowAltCircleLeft />
+            </div>
         </div>
     )
 }
