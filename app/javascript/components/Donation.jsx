cd8edb40ea7ade092d886dbfc55500be02d619ab
genxhero <genxhero@users.noreply.github.com>
2020-02-23 08:52:25 -0800

    build donation component

--- app/javascript/components/Donation.jsx
+++ app/javascript/components/Donation.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import { PayPalButton } from "react-paypal-button-v2";
 
 const Donation = props => {
     return (
         <div className="donation">
-
+                <PayPalButton />
         </div>
     )
 }
