19603825bc4dd0f6ff936d931b7b3a6ae0678bc0
genxhero <genxhero@users.noreply.github.com>
2020-02-23 09:07:22 -0800

    create show button

--- app/javascript/components/Advertisement.jsx
+++ app/javascript/components/Advertisement.jsx
@@ -1,16 +1,28 @@
 import React, {useState} from 'react';
-import {FaArrowAltCircleLeft} from 'react-icons/fa';
+import {FaArrowAltCircleLeft, FaArrowAltCircleRight} from 'react-icons/fa';
 
 const Advertisement = props => {
      const [visible, toggleVisibility] = useState(true)
     return (
-        <div className="advertisement">
-            <div>Your Ad Here </div>
-            <div className={"hide-btn-left"} onClick={() => toggleVisibility(false)}>
-                <FaArrowAltCircleLeft />
-            </div>
-        </div>
-    )
+      <div className="advertisement">
+        <div>Your Ad Here </div>
+        {visible ? (
+          <div
+            className={"hide-btn-left"}
+            onClick={() => toggleVisibility(false)}
+          >
+            <FaArrowAltCircleLeft />
+          </div>
+        ) : (
+          <div
+            className={"hide-btn-left"}
+            onClick={() => toggleVisibility(true)}
+          >
+            <FaArrowAltCircleRight />
+          </div>
+        )}
+      </div>
+    );
 }
 
 export default Advertisement;
