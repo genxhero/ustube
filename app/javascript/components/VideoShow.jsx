1931d58991ad87133b73321e356f64bc3d27d35a
genxhero <genxhero@users.noreply.github.com>
2020-02-22 18:27:43 -0800

    create loading thing for videos

--- app/javascript/components/VideoShow.jsx
+++ app/javascript/components/VideoShow.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import { Player } from "video-react";
 
 const VideoShow = props => {
-    debugger;
-return <div>
-     
-</div>
+return (
+  <div>
+    <Player
+      playsInline
+      src="https://media.w3.org/2010/05/sintel/trailer_hd.mp4"
+    />
+  </div>
+);
 }
 
 export default VideoShow;
