27d398099d23a490d896775f7dd680d337c72b63
genxhero <genxhero@users.noreply.github.com>
2020-02-22 21:02:19 -0800

    separate the component into header, middle, and bottom

--- app/javascript/components/VideoShow.jsx
+++ app/javascript/components/VideoShow.jsx
@@ -5,6 +5,9 @@ const VideoShow = props => {
     debugger;
 return (
   <div className="video-show-page">
+      <div className="header">
+        <h1>US Tube - A Platform for All of Us</h1>
+      </div>
     <div className="middle-content">
       <div className="video-container">
         <Player
@@ -12,11 +15,20 @@ return (
           src="https://media.w3.org/2010/05/sintel/trailer_hd.mp4"
         />
       </div>
-      <div className="comments-section">
-            <p>
-                Comments will go here when implemented.
-            </p>
+      <div className="bottom-content">
+        <div className="ratings">
+                    <span>Star ratings will go here</span>
+            </div>
+            <div className="video-information">
+                    <span>VIdeo information will go here</span>
+            </div>
+            <div className="comments-section">
+                    <p>
+                        Comments will go here when implemented.
+                    </p>
+            </div>
       </div>
+     
     </div>
   </div>
 );
