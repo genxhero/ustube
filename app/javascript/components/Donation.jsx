f69bc65ec7f63db441f6257f47eaf8e043110f9c
genxhero <genxhero@users.noreply.github.com>
2020-02-23 08:54:19 -0800

    style the donation section

--- app/javascript/components/Donation.jsx
+++ app/javascript/components/Donation.jsx
@@ -4,6 +4,8 @@ import { PayPalButton } from "react-paypal-button-v2";
 const Donation = props => {
     return (
         <div className="donation">
+            <p>Ads don't pay for everything - if you really like this website please donate using the buttons below.</p>
+            <p></p>
                 <PayPalButton />
         </div>
     )
