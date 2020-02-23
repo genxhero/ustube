1ced9dbc3c93657533fb2cab04652f5514d4a38a
genxhero <genxhero@users.noreply.github.com>
2020-02-22 20:59:30 -0800

    lay out the page layout

--- app/javascript/components/VideoShow.jsx
+++ app/javascript/components/VideoShow.jsx
@@ -2,12 +2,22 @@ import React from 'react';
 import { Player } from "video-react";
 
 const VideoShow = props => {
+    debugger;
 return (
-  <div className="video-container">
-    <Player
-      playsInline
-      src="https://media.w3.org/2010/05/sintel/trailer_hd.mp4"
-    />
+  <div className="video-show-page">
+    <div className="middle-content">
+      <div className="video-container">
+        <Player
+          playsInline
+          src="https://media.w3.org/2010/05/sintel/trailer_hd.mp4"
+        />
+      </div>
+      <div className="comments-section">
+            <p>
+                Comments will go here when implemented.
+            </p>
+      </div>
+    </div>
   </div>
 );
 }
