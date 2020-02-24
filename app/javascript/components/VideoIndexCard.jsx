79244f4ff7f7789f001db0905d82b0b7a84233b7
genxhero <genxhero@users.noreply.github.com>
2020-02-24 07:51:26 -0800

    create an index card component for further abstraction

---
+++ app/javascript/components/VideoIndexCard.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const VideoIndexCard = (props) => {
+    const {video} = props;
+    return <div>
+        Getting to it
+    </div>
+}
+
+export default VideoIndexCard;
