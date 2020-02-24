e8935b9c1b7fa290cc9d6e46094b0b953063c4fd
genxhero <genxhero@users.noreply.github.com>
2020-02-24 07:48:43 -0800

    more styles to donation component

--- app/javascript/components/Donation.jsx
+++ app/javascript/components/Donation.jsx
@@ -5,7 +5,7 @@ const Donation = props => {
     return (
         <div className="donation">
             <p>Ads don't pay for everything - if you really like this website please donate using the buttons below.</p>
-            <p></p>
+            <p>TODO: Figure out how to send 90% of the money donated to the content creators.</p>
             <PayPalButton />
         </div>
     )
