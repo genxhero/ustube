14dcefab56572160683fbf5cd38439f42f7545a3
genxhero <genxhero@users.noreply.github.com>
2020-02-25 23:11:01 -0800

    comments section

---
+++ app/javascript/components/CommentsSection.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CommentsSection = props => {
+    return (
+        <div className="video-comments">
+
+        </div>
+    )
+}
+
+export default CommentsSection;
