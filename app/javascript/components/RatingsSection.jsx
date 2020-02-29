69ffa2b131b8140ebe4d59f7950c97f07f760f25
genxhero <genxhero@users.noreply.github.com>
2020-02-29 09:24:35 -0800

    usestate and stuff you know the drill

--- app/javascript/components/RatingsSection.jsx
+++ app/javascript/components/RatingsSection.jsx
@@ -1,13 +1,24 @@
-import React from 'react';
+import React, {useState} from 'react';
 import StarRatingComponent from 'react-star-rating-component';
 
 
 const RatingsSection = props => {
+
+    const [rating, setRating] = useState(null)
+    const onStarClick = (nextValue, prevValue, name) => {
+        setRating(nextValue)
+    }
+
     return (
-        <div className="ratings-section">
-            Star Ratings here.
-        </div>
-    )
+      <div className="ratings-section">
+        <StarRatingComponent
+          name="rate1"
+          starCount={10}
+          value={rating}
+          onStarClick={onStarClick}
+        />
+      </div>
+    );
 }
 
 export default RatingsSection;
