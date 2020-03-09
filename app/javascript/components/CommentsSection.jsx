e07fc7d8251d98067621820dae1de5b3145a7256
genxhero <genxhero@users.noreply.github.com>
2020-03-08 23:42:19 -0700

    comments for comment section

--- app/javascript/components/CommentsSection.jsx
+++ app/javascript/components/CommentsSection.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import Comment from './Comment';
 
+/**
+ * Comments section. 
+ * TODO: Something to do with threading the comments. Like maybe show the replies of the comments?
+ */
+
 const CommentsSection = props => {
     return (
         <div className="video-comments">
