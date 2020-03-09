0a20365183fb203f190a93051cc16810540b4a69
genxhero <genxhero@users.noreply.github.com>
2020-03-08 23:41:13 -0700

    comment for comment

--- app/javascript/components/Comment.jsx
+++ app/javascript/components/Comment.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
-
+/**
+ * 
+ * Exactly what it says on the tin; shows a deletable comment. can also be replied to. comments on the same thread will appear nested together
+ */
 const Comment = props => {
     return (
         <div className="comment">
