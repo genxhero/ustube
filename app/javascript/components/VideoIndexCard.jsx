835b5c2f3e69b1b9628751324438023724e95c72
genxhero <genxhero@users.noreply.github.com>
2020-03-08 23:46:32 -0700

    comment for video index card

--- app/javascript/components/VideoIndexCard.jsx
+++ app/javascript/components/VideoIndexCard.jsx
@@ -1,5 +1,11 @@
 import React from 'react';
 
+/**
+ * Index component showing a thumbnail and description for a video
+ * 
+ * @param {*} props 
+ */
+
 const VideoIndexCard = (props) => {
     const {video} = props;
     return (
