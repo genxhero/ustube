db5d2c366610cb307ea46e4effe9e230a86b3f53
genxhero <genxhero@users.noreply.github.com>
2021-01-08 10:09:28 -0800

    Flesh out the center of the div

--- app/javascript/components/VideoInformation.jsx
+++ app/javascript/components/VideoInformation.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 const VideoInformation = (props) => {
-    const {currentUser, creator, information} = props
+    const {currentUser, creator, description} = props
     return (
-        <div>
-            
+        <div className="video-information">
+              <p>{description}</p>  
         </div>
     )
 }
