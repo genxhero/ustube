557e2ff018ce2ea9081cc321df69e9e06f040756
genxhero <genxhero@users.noreply.github.com>
2021-01-08 10:25:39 -0800

    add video thumbnail

--- app/javascript/components/VideoIndexCard.jsx
+++ app/javascript/components/VideoIndexCard.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import VideoThumbnail from 'react-video-thumbnail'; 
 
 /**
  * Index component showing a thumbnail and description for a video
@@ -10,7 +11,13 @@ const VideoIndexCard = (props) => {
     const {video} = props;
     return (
         <div className="video-index-card">
-            Getting to it
+            <VideoThumbnail 
+              videoUrl={video.video_url}
+              thumbnailHandler={(thumbnail) => console.log(thumbnail)}
+              width={120}
+              height={80}
+              />
+            <h3>{video.title}</h3>
         </div>
     )
 }
