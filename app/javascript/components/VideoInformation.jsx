04cad93eddb6004a3cc8171099ac37dd7224d32c
genxhero <genxhero@users.noreply.github.com>
2021-01-08 10:06:29 -0800

    create vid information component

---
+++ app/javascript/components/VideoInformation.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const VideoInformation = (props) => {
+    const {currentUser, creator, information} = props
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default VideoInformation;
