a3a435d8ce52e8a9d9817956c9e51b0ac9a3ae8a
genxhero <genxhero@users.noreply.github.com>
2021-02-13 21:14:38 -0800

    generate migration

--- app/javascript/components/VideoIndexCard.jsx
+++ app/javascript/components/VideoIndexCard.jsx
@@ -11,12 +11,11 @@ import VideoThumbnail from 'react-video-thumbnail';
 
 const VideoIndexCard = (props) => {
     const {video} = props;
+    console.log(video)
     return (
         <div className="video-index-card">
             <h3>{video.title}</h3>
             <img className="video-thumbnail" src={video.image_url}/>
-
-            <h3>{video.title}</h3>        
         </div>
     )
 }
---
+++ db/migrate/20210214051359_add_timestamps_to_videos.rb
@@ -0,0 +1,4 @@
+class AddTimestampsToVideos < ActiveRecord::Migration[6.0]
+  def change
+  end
+end
