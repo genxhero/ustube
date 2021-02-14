824ed0e3e4c59885bb82d21d703328543f081c63
genxhero <genxhero@users.noreply.github.com>
2021-02-13 21:24:09 -0800

    finalize the new columns

--- db/migrate/20210214051359_add_timestamps_to_videos.rb
+++ db/migrate/20210214051359_add_timestamps_to_videos.rb
@@ -1,6 +1,6 @@
 class AddTimestampsToVideos < ActiveRecord::Migration[6.0]
   def change
-      add_column :videos, :created_at, :datetime, null: false
-      add_column :videos, :updated_at, :datetime, null: false
+      add_column :videos, :created_at, :datetime, null: true
+      add_column :videos, :updated_at, :datetime, null: true
   end
 end
