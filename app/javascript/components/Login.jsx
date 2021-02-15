61c241cff13eef4037bde1ebda6918b3894eec01
genxhero <genxhero@users.noreply.github.com>
2021-02-14 22:46:48 -0800

    create buttons and fields for login

--- app/javascript/components/Login.jsx
+++ app/javascript/components/Login.jsx
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const Login = props => {
+
+    const [credentials, updateCredentials] = useState({email: "", password: ""})
+
     return (
         <div className="session-form">
-
+            <input className="session-field" />
+            <input className="session-field" />
+            <button className="session-button" />
         </div>
     );
 }
