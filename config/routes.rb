3a691f46f28e969148b0498a918364f1444235fc
genxhero <genxhero@users.noreply.github.com>
2021-02-14 22:57:36 -0800

    fiddle with routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   resources :videos
   resources :sessions, only: [:create, :destroy]
-  resources :users
+  post '/login', to: 'sessions#create'
+#  delete '/logout', to: 'sessions#destroy'
+ # get '/logged_in', to: 'sessions#is_logged_in?'
+  resources :users, only: [:create, :show, :index]
 end
