8901c789fa298225b8ee581506d55f3de83fb1dd
genxhero <genxhero@users.noreply.github.com>
2021-02-15 22:49:02 -0800

    proper values

--- app/javascript/components/Register.jsx
+++ app/javascript/components/Register.jsx
@@ -5,17 +5,21 @@ const Register = props => {
 
 
     const handleChange = e => {
-        console.log("the function isbeing called")
+        console.log(user)
+    }
+
+    const handleSubmit = e => {
+        console.log("trybnna submit")
     }
 
     return (
         <div className="session-form">
             <h1>Register</h1>
-                <form onChange={handleChange}>
-                    <input className="session-field" />
-                    <input className="session-field" />
-                    <input className="session-field" />
-                    <input className="session-field" />
+                <form onChange={handleChange} onSubmit={handleSubmit}>
+                    <input className="session-field" value={user.username}/>
+                    <input className="session-field" value={user.email}/>
+                    <input className="session-field" value={user.password}/>
+                    <input className="session-field" value={user.passwordConfirm}/>
                     <input type="submit" className="session-button" />
                 </form>
         </div>
