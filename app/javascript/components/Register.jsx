c3e0e0e9ed0e2092924978948a5a7826609c656d
genxhero <genxhero@users.noreply.github.com>
2021-02-15 22:45:24 -0800

    wrap inputs in form

--- app/javascript/components/Register.jsx
+++ app/javascript/components/Register.jsx
@@ -2,15 +2,22 @@ import React, {useState} from 'react';
 
 const Register = props => {
     const [user, updateUser] = useState({username: "", email: "", password: "", passwordConfirm: ""})
-    
+
+
+    const handleChange = e => {
+
+    }
+
     return (
         <div className="session-form">
             <h1>Register</h1>
-                <input className="session-field" />
-                <input className="session-field" />
-                <input className="session-field" />
-                <input className="session-field" />
-                <input type="submit" className="session-button" />
+                <form>
+                    <input className="session-field" />
+                    <input className="session-field" />
+                    <input className="session-field" />
+                    <input className="session-field" />
+                    <input type="submit" className="session-button" />
+                </form>
         </div>
     );
 }
