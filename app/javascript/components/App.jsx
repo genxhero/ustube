95176971773f9b96a53bb3f1da5ade44672b5658
genxhero <genxhero@users.noreply.github.com>
2021-02-18 22:28:38 -0800

    main app skeleton

---
+++ app/javascript/components/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import {BrowserRouter, Switch} from 'react-router';
+
+const App = props => {
+
+    return (
+        <BrowserRouter>
+            <Switch>
+
+            </Switch>
+        </BrowserRouter>
+    )
+
+}
+
+export default App;
