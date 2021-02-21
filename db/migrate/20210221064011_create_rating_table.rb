a8c405af6210e76797c16ce1dc7d1839f0c0216a
genxhero <genxhero@users.noreply.github.com>
2021-02-20 22:42:32 -0800

    flesh out migration

--- db/migrate/20210221064011_create_rating_table.rb
+++ db/migrate/20210221064011_create_rating_table.rb
@@ -1,6 +1,12 @@
 class CreateRatingTable < ActiveRecord::Migration[6.0]
   def change
-    create_table :rating_tables do |t|
+    create_table :ratings do |t|
+       t.integer :user_id, null: false
+       t.integer :video_id, null: false
+       t.integer :value, null: false
+       t.timestamps
     end
+       add_index :ratings, :user_id
+       add_index :ratings, :video_id
   end
 end
