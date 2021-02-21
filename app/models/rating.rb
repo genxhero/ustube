5b8e35d0eb57c1434dec26ff070f5428703a1a11
genxhero <genxhero@users.noreply.github.com>
2021-02-20 22:47:44 -0800

    model for rating

---
+++ app/models/rating.rb
@@ -0,0 +1,13 @@
+class Rating < ApplicationRecord
+
+    belongs_to :user,
+    primary_key: :id,
+    foreign_key: :user_id,
+    class_name: "User"
+
+    belongs_to :video,
+    primary_key: :id,
+    foreign_key: :video_id,
+    class_name: "Video"
+
+end
